d65ca030359851dec7bae19400949b974d6b9ca8
carbafra <carbafra@011-4303-S03MR.students.cisd.conroeisd.loc>
2018-10-31 08:08:18 -0500

    Added a print statement to end your statement and changed String To Arr

--- Input/src/input/Input.java
+++ Input/src/input/Input.java
@@ -24,16 +24,16 @@ public class Input {
           Scanner scan = new Scanner(new File("stuff.txt"));
           
           // Create an Array
-          String[] stringArr = new String[5];
+          String[] Arr = new String[5];
           
           // Take input line-by-line and store it in arr[]
-          stringArr[0] = scan.nextLine();
-          stringArr[1] = scan.nextLine();
-          stringArr[2] = scan.nextLine();
-          stringArr[3] = scan.nextLine();
-          stringArr[4] = scan.nextLine();
+          Arr[0] = scan.nextLine();
+          Arr[1] = scan.nextLine();
+          Arr[2] = scan.nextLine();
+          Arr[3] = scan.nextLine();
+          Arr[4] = scan.nextLine();
           
-          out.printf("%s %s %s %s %s", stringArr[0], stringArr[1], stringArr[2], stringArr[3], stringArr[4]);
+          out.printf("%s %s %s %s %s", Arr[0], Arr[1], rr[2], Arr[3], Arr[4]);
           
           
           
